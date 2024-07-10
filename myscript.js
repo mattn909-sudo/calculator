cb7e792999d1ec0237d04766d98430f5ecf818b9
Matt Nguyen <mattn9091@gmail.com>
2024-07-10 14:00:46 -0400

    Change index divs to buttons and add operator func

--- myscript.js
+++ myscript.js
@@ -1,3 +1,8 @@
+let valueA = 0;
+let valueB = 0;
+let operator = '';
+let perfNextOp = false;
+
 function add(a, b){
     return a + b;
 }
@@ -7,9 +12,22 @@ function sub(a, b){
 }
 
 function divide(a, b){
-    return a/b;
+    return a / b;
 }
 
 function multiply(a,b){
     return a * b;
+}
+
+function operate(a, b, op){
+    switch(op){
+        case '+':
+            return add(a, b);
+        case '-': 
+            return sub(a, b);
+        case '*': 
+            return multiply(a, b);
+        case '/': 
+            return divide(a, b);
+    }
 }
