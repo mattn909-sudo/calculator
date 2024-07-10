5c62e1366131da9998e308f79acd5c9b847b6a80
Matt Nguyen <mattn9091@gmail.com>
2024-07-10 15:21:32 -0400

    Add logic for calulator responding to button press

--- myscript.js
+++ myscript.js
@@ -2,43 +2,80 @@ let valueA = '';
 let valueB = '';
 let operator = '';
 let perfNextOp = false;
+let readyToEquate = false;
 let screenText ='';
+const NUMERICAL = '1234567890'
+const OPERATOR = '+-/*'
 const allButtons = document.querySelector('.calcContainer');
 const screen = document.querySelector(".screenRow");
 
 allButtons.addEventListener('click',(event) => {
-    let target = event.target;
-    if(target.tagName == 'BUTTON'){
-        screenText += target.textContent;
-        screen.textContent = screenText;
+    selected = event.target.textContent;
+    if(NUMERICAL.includes(selected)){
+        if(perfNextOp && valueB.length < 9){
+            valueB += selected;
+            screenText = valueB;
+            readyToEquate = true;
+        }
+        else if(valueA.length < 9){
+            valueA += selected;
+            screenText = valueA;
+        }
     }
-    
-})
+    else if(OPERATOR.includes(selected)){
+        if(perfNextOp){
+            valueA = operate(valueA, valueB, selected);
+            valueB = '';
+            screenText = valueA;
+        }
+        else{
+            operator = selected
+            perfNextOp = true;
+        }
+    }
+    else if(selected == '=' && perfNextOp && readyToEquate){
+        valueA = operate(valueA, valueB, operator);
+        screenText = valueA;
+        valueB = '';
+    }
+    else if(selected == 'AC'){
+        valueA = '';
+        valueB = '';
+        operator = '';
+        screenText = '';
+        perfNextOp = false;
+        readyToEquate = false;
+    }
+    screen.textContent = screenText;
+
+});
 function add(a, b){
-    return a + b;
+    return (a + b).toString();
 }
 
 function sub(a, b){
-    return a - b;
+    return (a - b).toString();
 }
 
 function divide(a, b){
-    return a / b;
+    return (a / b).toString();
 }
 
 function multiply(a,b){
-    return a * b;
+    return (a * b).toString();
 }
 
 function operate(a, b, op){
+    let intA = Number(a);
+    let intB = Number(b);
     switch(op){
         case '+':
-            return add(a, b);
+            return add(intA, intB);
         case '-': 
-            return sub(a, b);
+            return sub(intA, intB);
         case '*': 
-            return multiply(a, b);
+            return multiply(intA, intB);
         case '/': 
-            return divide(a, b);
+            return divide(intA, intB);
     }
 }
